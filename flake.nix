f35f128899d676731be5de94f9971b6a8ec5547c
Maix0 <maix522@gmail.com>
2024-06-15 18:48:30 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     starship = {
-      url = "github:maix0/starship-flake";
+      url = "github:maix0/starship";
       flake = false;
     };
   };
@@ -21,32 +21,80 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
       naersk' = pkgs.callPackage inputs.naersk {};
-      starship = naersk'.buildPackage {src = inputs.starship-flake;};
+      starship = naersk'.buildPackage {
+        src = inputs.starship;
+        buildInputs = [pkgs.cmake];
+      };
       starship_config = builtins.readFile ./starship_config.toml;
       zshrc_data = ''
+        [ -f "$HOME/.zshenv" ] && source "$HOME/.zshenv";
+        [ -f "$HOME/.zvars"  ] && source "$HOME/.zvars";
 
-        [ -f "$HOME/.zshenv" ] && source "$HOME/.zshenv"
-        [ -f "$HOME/.zvars"  ] && source "$HOME/.zvars"
+        ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
+        [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+        [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+        source "''${ZINIT_HOME}/zinit.zsh"
 
-        #export STARSHIP_CONFIG="${pkgs.writeTextFile "starship-config.toml" starship_config}"
+        zinit light z-shell/F-Sy-H
+        zinit light zsh-users/zsh-syntax-highlighting
+        zinit light zsh-users/zsh-completions
+        zinit light zsh-users/zsh-autosuggestions
+        zinit light Aloxaf/fzf-tab
+        zinit light nix-community/nix-zsh-completions
+
+        zinit snippet OMZP::git
+        zinit snippet OMZP::sudo
+
+        autoload -Uz compinit && compinit
+
+        SAVEHIST=$HISTSIZE
+        HISTDUP=erase
+        setopt appendhistory
+        setopt sharehistory
+        setopt hist_ignore_space
+        setopt hist_ignore_all_dups
+        setopt hist_save_no_dups
+        setopt hist_ignore_dups
+        setopt hist_find_no_dups
+
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+        zstyle ':completion:*' menu no
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+
+        export STARSHIP_CONFIG="${pkgs.writeTextFile {
+          name = "starship-config.toml";
+          text = starship_config;
+        }}"
         eval "$(${starship}/bin/starship init zsh)"
+        eval "$(direnv hook zsh)"
+        eval "$(fzf --zsh)"
+        eval "$(zoxide init --cmd cd zsh)"
       '';
+      zsh_config_file = pkgs.writeTextFile {
+        name = ".zshrc";
+        text = zshrc_data;
+        destination = "/.zshrc";
+      };
     in {
       packages = rec {
+        inherit starship;
         default = zsh;
-        zsh = pkgs.mkDerivation {
-          name = "zsh";
-          version = "0.0.0";
-        };
+        zsh =
+          pkgs.writeScriptBin "zsh"
+          ''
+            ZDOTDIR="${zsh_config_file}" ${pkgs.zsh} "$@"
+          '';
       };
 
-      app = rec {
+      apps = rec {
         default = zsh;
-        zsh = flake-utils.mkApp {
+        zsh = flake-utils.lib.mkApp {
           name = "zsh";
           drv = self.packages.${system}.zsh;
         };
-        install = flake-utils.mkApp {};
       };
       #   home.packages = [pkgs.nix-zsh-completions];
       #   programs = {
