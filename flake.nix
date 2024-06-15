224c05d1eb5ff6e18da5a06e9eade29ba2b5ab84
Maix0 <maix522@gmail.com>
2024-06-15 18:13:29 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -3,26 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    dotacat = {
-      url = "git+https://gitlab.scd31.com/stephen/dotacat.git";
-      flake = false;
-    };
-    naersk = {
-      url = "github:nix-community/naersk";
-    };
-    fast-syntax-highlighting = {
-      url = "github:z-shell/F-Sy-H";
-      flake = false;
-    };
-    zsh-nix-shell = {
-      url = "github:chisui/zsh-nix-shell";
-      flake = false;
-    };
-    powerlevel10k = {
-      url = "github:romkatv/powerlevel10k";
-      flake = false;
-    };
+    naersk.url = "github:nix-community/naersk";
     flake-utils.url = "github:numtide/flake-utils";
+
+    starship = {
+      url = "github:maix0/starship-flake";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -31,85 +18,90 @@
     flake-utils,
     ...
   } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: {
-      home-managerModule = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        naersk-lib = inputs.naersk.lib."${system}";
-        dotacat = naersk-lib.buildPackage {
-          pname = "dotacat";
-          root = inputs.dotacat;
-        };
-      in {
-        home.packages = [pkgs.nix-zsh-completions];
-        programs = {
-          nix-index = {
-            enable = true;
-            enableZshIntegration = true;
-          };
-          zsh = {
-            enable = true;
-            enableCompletion = true;
-            oh-my-zsh = {
-              enable = true;
-              plugins = ["git" "wd" "rust"];
-            };
-            plugins = [
-              {
-                name = "F-Sy-H";
-                file = "F-Sy-H.plugin.zsh";
-                src = inputs.fast-syntax-highlighting;
-              }
-              {
-                name = "zsh-nix-shell";
-                file = "nix-shell.plugin.zsh";
-                src = inputs.zsh-nix-shell;
-              }
-            ];
-            initExtra = ''
-              export PATH="$PATH:$HOME/bin"
-              source ~/.p10k.zsh
-              source ~/.powerlevel10k/powerlevel10k.zsh-theme
-              if [ -f "$HOME/.zvars" ]; then
-                source "$HOME/.zvars"
-              fi
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      naersk' = pkgs.callPackage inputs.naersk {};
+      starship = naersk'.buildPackage {src = inputs.starship-flake;};
+      starship_config = builtins.readFile ./starship_config.toml;
+      zshrc_data = ''
 
-              if [ -f "$HOME/.localrc.sh" ]; then
-                source "$HOME/.localrc.sh"
-              fi
+        [ -f "$HOME/.zshenv" ] && source "$HOME/.zshenv"
+        [ -f "$HOME/.zvars"  ] && source "$HOME/.zvars"
 
-              export PATH="${config.home.homeDirectory}/bin:$PATH"
-
-              ${pkgs.fortune}/bin/fortune \
-                | ${pkgs.cowsay}/bin/cowsay \
-                | ${dotacat}/bin/dotacat
-            '';
-            shellAliases = {
-              cat = "${pkgs.bat}/bin/bat -p";
-              ls = "${pkgs.exa}/bin/exa --icons";
-              py3 = "nix-shell -p python3 python3.pkgs.matplotlib --run python3";
-              vim = "nvim";
-            };
-          };
-        };
-
-        home.file = {
-          ".powerlevel10k".source = inputs.powerlevel10k;
-          ".zprofile".source = ./zprofile;
-          ".p10k.zsh".source = ./p10k.zsh;
-          "bin/ns" = {
-            text = ''
-              #!/usr/bin/env bash
-
-              set -- "''${@/#/nixpkgs#}"
-              nix shell "$@"
-            '';
-            executable = true;
-          };
+        #export STARSHIP_CONFIG="${pkgs.writeTextFile "starship-config.toml" starship_config}"
+        eval "$(${starship}/bin/starship init zsh)"
+      '';
+    in {
+      packages = rec {
+        default = zsh;
+        zsh = pkgs.mkDerivation {
+          name = "zsh";
+          version = "0.0.0";
         };
       };
+
+      app = rec {
+        default = zsh;
+        zsh = flake-utils.mkApp {
+          name = "zsh";
+          drv = self.packages.${system}.zsh;
+        };
+        install = flake-utils.mkApp {};
+      };
+      #   home.packages = [pkgs.nix-zsh-completions];
+      #   programs = {
+      #     nix-index = {
+      #       enable = true;
+      #       enableZshIntegration = true;
+      #     };
+      #     zsh = {
+      #       enable = true;
+      #       enableCompletion = true;
+      #       oh-my-zsh = {
+      #         enable = true;
+      #         plugins = ["git" "wd" "rust"];
+      #       };
+      #       plugins = [
+      #         {
+      #           name = "F-Sy-H";
+      #           file = "F-Sy-H.plugin.zsh";
+      #           src = inputs.fast-syntax-highlighting;
+      #         }
+      #         {
+      #           name = "zsh-nix-shell";
+      #           file = "nix-shell.plugin.zsh";
+      #           src = inputs.zsh-nix-shell;
+      #         }
+      #       ];
+      #       initExtra = ''
+      #         export PATH="$PATH:$HOME/bin"
+      #         source ~/.p10k.zsh
+      #         source ~/.powerlevel10k/powerlevel10k.zsh-theme
+      #         if [ -f "$HOME/.zvars" ]; then
+      #           source "$HOME/.zvars"
+      #         fi
+      #
+      #         if [ -f "$HOME/.localrc.sh" ]; then
+      #           source "$HOME/.localrc.sh"
+      #         fi
+      #
+      #         export PATH="${config.home.homeDirectory}/bin:$PATH"
+      #
+      #         ${pkgs.fortune}/bin/fortune \
+      #           | ${pkgs.cowsay}/bin/cowsay \
+      #           | ${pkgs.dotacat}/bin/dotacat
+      #       '';
+      #       shellAliases = {
+      #         cat = "${pkgs.bat}/bin/bat -p";
+      #         ls = "${pkgs.exa}/bin/exa --icons -a";
+      #         vim = "nvim";
+      #       };
+      #     };
+      #   };
+      #
+      #   home.file = {
+      #     ".zprofile".source = ./zprofile;
+      #   };
+      # };
     });
 }
